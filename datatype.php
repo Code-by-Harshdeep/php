74af061882b1d9df7c37f3ab0b391cfcd6b9c500
Harsh <hs855787@gmail.com>
2026-07-11 20:37:42 +0530

    datatypes in php

--- datatype.php
+++ datatype.php
@@ -8,6 +8,12 @@
 <body>
     <marquee behavior="" direction="" >Learning</marquee>
     <?php 
+    $a=150;  //int
+    $b=23.56; //float
+    $c="Hello"; // String
+    $d=TRUE; //boolean but always need to write in capital
+    $arr=array("Ludhiana","Ferozpur","Bathinda");  //array
+    $e=null; //null data type
     /*
     Data types in PHP
 
