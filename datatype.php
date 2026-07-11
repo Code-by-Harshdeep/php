d6be035369d304acbe592b56f9284db941d5d34f
Harsh <hs855787@gmail.com>
2026-07-11 20:33:32 +0530

    basic of php

---
+++ datatype.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Learning php</title>
+</head>
+<body>
+    <marquee behavior="" direction="" >Learning</marquee>
+    <?php 
+    /*
+    Data types in PHP
+
+    int
+    float
+    string
+    boolean
+    array
+    object
+    
+    
+    */
+
+?>
+</body>
+</html>
---
+++ test.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Learning php</title>
+</head>
+<body>
+    <marquee behavior="" direction="" >Learning</marquee>
+    <?php 
+  echo "<font color=red size=5>Lets Learn Php</font>";
+  print("<br>This is new line <br>");
+  $var=100;
+  $var2=50;
+  $var3=$var+$var2;
+  echo "addition of $var and $var2 is $var3"
+?>
+</body>
+</html>
