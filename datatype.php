bc668faa910a3ce8e2a37effc7aed43589443b4a
Harsh <hs855787@gmail.com>
2026-07-11 20:56:10 +0530

    conditional statement done  with variables

--- datatype.php
+++ datatype.php
@@ -8,12 +8,25 @@
 <body>
     <marquee behavior="" direction="" >Learning</marquee>
     <?php 
+    class Test{
+
+    }
     $a=150;  //int
+    echo $a."<br>";
     $b=23.56; //float
+    echo $b."<br>";
     $c="Hello"; // String
+    echo $c."<br>";
     $d=TRUE; //boolean but always need to write in capital
+    echo $d."<br>";
     $arr=array("Ludhiana","Ferozpur","Bathinda");  //array
+   //function to print array is print_r
+   print_r($arr);
     $e=null; //null data type
+    echo $e."<br>";
+    // t=new Test();
+
+
     /*
     Data types in PHP
 
@@ -23,9 +36,17 @@
     boolean
     array
     object
-    
-    
+
     */
+
+      $num1=150; 
+      $num2=100; 
+      if($num1>$num2){
+        echo "Number 1 is Greater : ".$num1;
+      }
+      else{
+        echo "Number 2 is Greater";
+      }
 
 ?>
 </body>
