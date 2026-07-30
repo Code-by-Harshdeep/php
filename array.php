d5f30b87739051879e80c557e3a13932e23d13de
Harsh <hs855787@gmail.com>
2026-07-30 22:27:52 +0530

    array declaration and reassignment of value at index done

---
+++ array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Arrays</title>
+</head>
+<body>
+    <?php
+    $cars=["Toyota","Ferrari","Porsche"];
+    print_r ($cars);
+    print_r ($cars[1]);
+    $cars[2]="BMW";
+    print_r ($cars);
+    ?>
+</body>
+</html>
