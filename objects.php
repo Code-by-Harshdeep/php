71755653d5183eaaca0d4134919096974b677cce
Harsh <hs855787@gmail.com>
2026-07-30 22:32:07 +0530

    objecrs decaration done and assign value to object property

---
+++ objects.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Objects</title>
+</head>
+<body>
+    <?php
+   class Car{
+         public $color;
+         public $brand;
+    }
+    $car1=new Car();
+    $car1->color="Red";
+    $car1->brand="Merecedes";
+
+    echo $car1->color;
+    echo "<br>";
+    echo $car1->brand;
+
+    ?>
+</body>
+</html>
