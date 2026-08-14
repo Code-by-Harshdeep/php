9cb8b8725b81fc242137461536501013b665c02d
Harsh <hs855787@gmail.com>
2026-08-14 12:00:00 +0530

    Covered push ,pop,unshift and shift methods in array;

--- arraymethods.php
+++ arraymethods.php
@@ -10,11 +10,17 @@
     $names = ["Alice", "James", "Kevin"];
     echo $names[0];  //Accesing specific element in array by indexing
 
-   echo count($names); //Counting elements in array with count method
+    echo count($names); //Counting elements in array with count method
 
-   array_push($names, "Bob"); //inserting new element in array with array_push method
+    array_push($names, "Bob"); //inserting new element in array with array_push method in the ending
+    print_r($names);
 
-      print_r($names);
+    array_pop($names); //deleting existing element from the last of an array
+    print_r($names);
+    array_unshift($names, "JigglyPuff"); //Adding New element from the Starting of Array
+    print_r($names);
+    array_shift($names); //Removing Existing element from the Starting of Array
+    print_r($names);
     ?>
     
 </body>
